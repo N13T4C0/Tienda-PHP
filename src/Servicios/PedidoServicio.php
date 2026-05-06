b1b5d697ff9288c565b8bc40a91bd30999c3bbe0
Nieto <josenieto.alo@gmail.com>
2026-05-06 21:15:55 +0200

    cache quitada

--- src/Servicios/PedidoServicio.php
+++ src/Servicios/PedidoServicio.php
@@ -39,7 +39,7 @@ class PedidoServicio
                 $unidades = (int) $item['cantidad'];
 
                 $this->repoPedido->insertarLinea($idPedido, $producto, $unidades);
-                // restyar stock
+                // restar stock 
                 $this->repoProducto->restarStock((int) $producto['id'], $unidades);
                 
             }
