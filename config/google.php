f58c29ea7d694a42e640c22dc385a59dd43fe2b4
Raúl Verdejo <verdejomartinraul@gmail.com>
2026-05-12 18:49:19 +0200

    variables de google en .env y cargando desde el env

--- config/google.php
+++ config/google.php
@@ -1,6 +1,7 @@
 <?php
+
 return [
-    'client_id'     => '258546770546-mtdek4d9tj49q3fcd9uq0k6n0nm44ude.apps.googleusercontent.com',
-    'client_secret' => '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',
-    'redirect_uri'  => 'http://localhost' . URL_BASE . '/auth/googleCallback',
+    'client_id'     => $_ENV['GOOGLE_CLIENT_ID'] ?? null,
+    'client_secret' => $_ENV['GOOGLE_CLIENT_SECRET'] ?? null,
+    'redirect_uri'  => $_ENV['GOOGLE_REDIRECT_URI'] ?? null,
 ];
